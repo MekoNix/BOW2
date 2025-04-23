f51347efd01622dee4fc206e657e66c7bcf60292
MekoNix <64309709+MekoNix@users.noreply.github.com>
2025-04-23 18:03:40 +0000

    modified:   .gitignore 	modified:   README.md 	new file:   project/modules/abilities/abilities.py 	new file:   project/modules/build/build.py 	new file:   project/modules/explore/explore.py 	new file:   project/modules/welcome.py 	new file:   requements.txt

---
+++ project/modules/welcome.py
@@ -0,0 +1,11 @@
+from modules.abilities import abilities
+from modules.build import build
+from modules.explore import explore
+import streamlit as st
+
+class Page():
+    def __init__(self):
+            pass
+    def page():
+        if st.button("Test"):
+            st.warning("DA")
